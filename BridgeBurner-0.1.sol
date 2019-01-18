513a3e845f0c18613c2af173fe926f6760362480
Chaz Schmidt <11149889+ChazSchmidt@users.noreply.github.com>
2019-01-18 15:18:52 -0500

    Update BridgeBurner-0.1.sol

--- BridgeBurner-0.1.sol
+++ BridgeBurner-0.1.sol
@@ -9,6 +9,8 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract DateMaker is Streams{  // creates Dates (shared streams) and corresponds them to the streams[]
     uint256 IDcounter;  // used for dateID and corresponding streamID/might be replaced by streamNonce
+    address burnAddress = 0x0000000000000000000000000000000000000000;
+    
     
     
 }
@@ -17,15 +19,54 @@ contract Date {
     address host;
     address invited;
     mapping(address => uint256) depositedShare;
-    //
+    
+    //create function parameters
+//    address _sender,
+//    address _recipient,
+    address tokenAddress;
+    uint256 startBlock;
+    uint256 stopBlock;
+    uint256 payment;
+    uint256 interval;
     
     
-    constructor(address _invited) { // should pass all terms to later create stream
+    constructor(
+        address _invited, 
+        address _tokenAddress;
+        uint256 _startBlock;
+        uint256 _stopBlock;
+        uint256 _payment;
+        uint256 _interval;) { // should pass all terms to later create stream
         
-    host = msg.sender;
-    invited = _invited;
-        
-    stream = // collect stream terms to be verified
+        host = msg.sender;
+        invited = _invited;
+        tokenAddress = _tokenAddress;
+        startBlock = _startBlock;
+        stopBlock = _stopBlock;
+        payment = _payment;
+        interval = _interval;
+    }
+    
+    function getStreamTerms() returns( 
+        address _tokenAddress,
+        uint256 _startBlock,
+        uint256 _stopBlock,
+        uint256 _payment,
+        uint256 _interval) { // should pass all terms to later create stream
+        return (
+        address _tokenAddress,
+        uint256 _startBlock,
+        uint256 _stopBlock,
+        uint256 _payment,
+        uint256 _interval)
+    }
+    
+    function getInvited() returns (address _invited) {
+        return invited;
+    }
+    
+    function getShareofBalance(address _address) {
+        return depositedShare[_address];
     }
   
 }
